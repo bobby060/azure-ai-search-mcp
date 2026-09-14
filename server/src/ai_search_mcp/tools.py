9a4431af4f995d594023ad3a2d6d84492c0b8c16
bobby060 <scoutrpn@gmail.com>
2026-09-13 21:28:09 -0400

    fix: better tools

--- server/src/ai_search_mcp/tools.py
+++ server/src/ai_search_mcp/tools.py
@@ -9,35 +9,31 @@ from .models import SearchIndex
 # Initialize the MCP server
 mcp = FastMCP("weather")
 
-AUDIENCE="https://search.azure.us"
+AUDIENCE = "https://search.azure.us"
 
 service_endpoint = os.environ["AZURE_SEARCH_SERVICE_ENDPOINT"]
 index_name = os.environ["AZURE_SEARCH_INDEX_NAME"]
 key = os.environ["AZURE_SEARCH_API_KEY"]
 
-credential=DefaultAzureCredential(authority=AzureAuthorityHosts.AZURE_US)
+credential = DefaultAzureCredential(authority=AzureAuthorityHosts.AZURE_US)
 
-search_index_client = SearchIndexClient(
-    service_endpoint, credential, audience=AUDIENCE)
+search_index_client = SearchIndexClient(service_endpoint, credential, audience=AUDIENCE)
+
 
 def build_client(index_name: str):
-    #TODO: Add error handling for invalid index
-    return SearchClient(service_endpoint,
-                         index_name, credential, audience=AUDIENCE)
+    # TODO: Add error handling for invalid index
+    return SearchClient(service_endpoint, index_name, credential, audience=AUDIENCE)
 
 
 @mcp.tool()
 async def list_search_indexes() -> list[SearchIndex] | None:
-    """List all indexes available on the Azure AI Search instance
-    """
+    """List all indexes available on the Azure AI Search instance"""
     return [
-        SearchIndex(
-            i.name, i.description) 
-        for i 
-        in search_index_client.list_indexes]
+        SearchIndex(i.name, i.description) for i in search_index_client.list_indexes
+    ]
 
 
-#TODO: Add validation for query_Type
+# TODO: Add validation for query_Type
 @mcp.tool()
 async def query_index(index_name: str, query: str, query_type: str) -> list[dict]:
     """Search an index for a query.
@@ -47,11 +43,10 @@ async def query_index(index_name: str, query: str, query_type: str) -> list[dict
         query: search string
         query_type: either simple or semantic
     """
-    if (query_type not in ["simple", "semantic"]):
-        raise ValueError(f"query_type {query_type} is not valid."
-                          "Use either `simple` or `semantic`") 
+    if query_type not in ["simple", "semantic"]:
+        raise ValueError(
+            f"query_type {query_type} is not valid.Use either `simple` or `semantic`"
+        )
 
-    client=build_client(index_name)
-    return [ r for r in client.search(query, query_type=query_type)]
-    
-   
+    client = build_client(index_name)
+    return [r for r in client.search(query, query_type=query_type)]
