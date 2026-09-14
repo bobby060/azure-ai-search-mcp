5ce32f55f12f80699dceb8dd3b875acd373b15d9
bobby060 <scoutrpn@gmail.com>
2026-09-13 21:39:49 -0400

    feat: add api key

--- server/src/ai_search_mcp/main.py
+++ server/src/ai_search_mcp/main.py
@@ -6,10 +6,15 @@ from .tools import mcp
 HOST_NAME = os.getenv("SERVER_HOST_NAME")
 HOST_PORT = os.getenv("SERVER_HOST_PORT")
 
+AUTH_TOKEN = os.getenv("AUTH_TOKEN")
+
 
 def main():
     # Initialize and run the server with FastMCP
-    mcp.run(transport="http", host=HOST_NAME, port=HOST_PORT)
+    mcp.run(transport="http", 
+            host=HOST_NAME, 
+            port=HOST_PORT,
+            auth=AUTH_TOKEN)
 
 
 if __name__ == "__main__":
